25eb81ef090b0fa5ff5da16881d6807df7f11176
danielrubens <danielrubensdaniel@gmail.com>
2023-01-28 19:49:15 -0300

    cleanup workin on rtl

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import Todo from "./components/Todo";
 
 function App() {
-  const todos = [
-    {id:1, title: 'wash the dishes', completed: false},
-    {id:2, title: 'clean the house', completed: false},
-    {id:3, title: 'cook hamburguer', completed: true}
-  ]
   return (
     <div className="App">
-      {todos.map((todo) => (<Todo todo={todo}/>))}
+      <Todo />
     </div>
   );
 }
--- client/src/__tests__/Todo.test.js
+++ client/src/__tests__/Todo.test.js
@@ -1,9 +1,25 @@
 import {render, screen, cleanup} from '@testing-library/react'
 import Todo from '../components/Todo';
 
-it('should render Todo component', () => {
-    render(<Todo/>)
-    const todoElement = screen.getByTestId('todo-1');
-    expect(todoElement).toBeInTheDocument();
-    expect(todoElement).toHaveTextContent('Todo');
-});
+afterEach(() => cleanup())
+
+describe('Testing the Todo component', () => {
+    it('should retrieve data-testid for todo-1', () => {
+        render(<Todo/>)
+        const todoElement = screen.getByTestId("todo-1");
+        expect(todoElement).toBeInTheDocument();
+        expect(todoElement).toHaveTextContent('wash the dishes');
+    });
+    it('should retrieve data-testid for todo-2', () => {
+        render(<Todo/>)
+        const todoElement = screen.getByTestId("todo-2");
+        expect(todoElement).toBeInTheDocument();
+        expect(todoElement).toHaveTextContent('clean the house');
+    });
+    it('should retrieve data-testid for todo-3', () => {
+        render(<Todo/>)
+        const todoElement = screen.getByTestId("todo-3");
+        expect(todoElement).toBeInTheDocument();
+        expect(todoElement).toHaveTextContent('cook hamburguer');
+    });
+})
